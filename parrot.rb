69714edc75d51f0079a35efef9ca03496a256deb
Esteban S Llerena <estebanllerena@gmail.com>
2019-12-10 17:51:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,17 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+
+#what are arguments
+
+name = "aysan"
+
+
+
+def exampla(name)
+  puts name
+end 
+
+example(name) 
+
